86bde239a7560ae38e088e915e07806a64b1e0df
saksham1122a <sakshamnnda01@gmail.com>
2026-08-07 16:54:26 +0530

    register api✅

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import morgan from 'morgan'
+import authRouter from '../src/routes/auth.routes.js'
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan("dev"));   // morgan is a logger generally tells about the request detials made to the server like method, on which api does it hit and how much response time did it took.
+
+app.use("/api/auth", authRouter)
+
+export default app
