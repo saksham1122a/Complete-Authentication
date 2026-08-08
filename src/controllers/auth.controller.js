2fcdcef92040a332a900cc7ff2a19ae091c747b9
saksham1122a <sakshamnnda01@gmail.com>
2026-08-08 11:55:46 +0530

    get-me api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -44,4 +44,29 @@ res.status(201).json({
 
 }
 
-    
+  export async function getMe(req,res){
+     
+// sabse pehle token ko header se nikalenge 
+// split("")[1] ka matlab hai joh postman ke header mein hum authorization field bnake value mein pehle Barrier space token paste karenge then yeh verify karega on the basis of token
+
+
+    const token = req.headers.authorization?.split(" ") [ 1 ];
+
+    if(!token){
+        res.status(401).json({
+            message:"token not found"
+        })
+    }
+
+    const decoded = jwt.verify(token, config.JWT_SECRET)
+
+    const user = await userModel.findById(decoded.id)
+    res.status(200).json({
+        message:"User fetched successfully!",
+        user: {
+            username: user.username,
+            email: user.email
+        }
+    })
+
+  }
