337d224c4d4926a2ee73e301066b3e2495479a3c
saksham1122a <sakshamnnda01@gmail.com>
2026-08-10 11:14:51 +0530

    gitignore

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -70,6 +70,71 @@ res.status(201).json({
 
 }
 
+  export async function login(req,res){
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email })
+
+    if(!user){
+        return res.status(400).json({
+            message:"Invalid email or password"
+        })
+    }
+
+    const hashedPassword = crypto.createHash("sha256").update(password).digest("hex")
+
+    const isPasswordValid = hashedPassword === user.password;
+
+    if(!isPasswordValid){
+        return res.status(400).json({
+            message:"Password incorrect!"
+        })  
+    }
+
+    const refreshToken = jwt.sign({
+        id:user._id,
+    }, config.JWT_SECRET,
+    {
+        expiresIn: "7d"
+    }
+)
+
+    const refreshTokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex")
+
+    const session = await sessionModel.create({
+        user:user._id,
+        refreshTokenHash,
+        ip:req.ip,
+        userAgent: req.headers["user-agent"]
+    })
+    
+    const accessToken = jwt.sign({
+        id:user._id,
+        sessionId:session._id
+    }, config.JWT_SECRET, 
+    {
+        expiresIn: "15m"
+    }
+)
+
+    res.cookie("refreshToken" , refreshToken,{
+        httpOnly:true,
+        secure:true,
+        sameSite: "strict",
+        maxAge: 7*24*60*60*1000 
+    })
+
+    res.status(200).json({
+        message:"User logged in successfully",
+        user: {
+            username: user.username,
+            email: user.email
+        },
+        accessToken
+    })
+
+  }
+
   export async function getMe(req,res){ // this api is created to fetched the details of a particular user like his username,email,password etc.
      
 // sabse pehle token ko header se nikalenge 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,7 +8,10 @@ const authRouter = Router();
  */
 authRouter.post('/register', authController.register)
 
-
+/**
+ * POST /api/auth/login
+ */
+authRouter.post('/login', authController.login)
 
 /**
  * GET /api/auth/get-me
